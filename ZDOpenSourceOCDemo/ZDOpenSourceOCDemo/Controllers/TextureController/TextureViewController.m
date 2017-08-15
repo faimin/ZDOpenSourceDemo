ff9e0cf3bcd5bad6a589c8684b4e945a3c04cd26
faimin <fuxianchao@gmail.com>
2017-08-15 20:11:10 +0800

    article link

---
+++ ZDOpenSourceOCDemo/ZDOpenSourceOCDemo/Controllers/TextureController/TextureViewController.m
@@ -0,0 +1,41 @@
+//
+//  TextureViewController.m
+//  ZDOpenSourceOCDemo
+//
+//  Created by Zero.D.Saber on 2017/8/15.
+//  Copyright © 2017年 Zero.D.Saber. All rights reserved.
+//
+//  Texture布局--快速开始：https://archimboldi.me/posts/%E7%BF%BB%E8%AF%91-texture-%E5%B8%83%E5%B1%80-%E5%BF%AB%E9%80%9F%E5%BC%80%E5%A7%8B-quickstart.html
+//  Texture布局--布局样例：https://archimboldi.me/posts/%E7%BF%BB%E8%AF%91-texture-%E5%B8%83%E5%B1%80-%E5%B8%83%E5%B1%80%E6%A0%B7%E4%BE%8B-layout-examples.html
+//  Texture布局--布局规格：https://archimboldi.me/posts/%E7%BF%BB%E8%AF%91-texture-%E5%B8%83%E5%B1%80-%E5%B8%83%E5%B1%80%E8%A7%84%E6%A0%BC-layout-specs.html
+
+#import "TextureViewController.h"
+#import <AsyncDisplayKit/AsyncDisplayKit.h>
+
+@interface TextureViewController ()
+
+@end
+
+@implementation TextureViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- ZDOpenSourceOCDemo/ZDOpenSourceOCDemo/Controllers/YogaController/YogaKitViewController.m
+++ ZDOpenSourceOCDemo/ZDOpenSourceOCDemo/Controllers/YogaController/YogaKitViewController.m
@@ -8,7 +8,7 @@
 //  布局说明:
 //  http://www.ruanyifeng.com/blog/2015/07/flex-grammar.html
 //  https://github.com/jakemarsh/core-layout
-//  https://github.com/mwlio/YogaDemo
+//  Yoga 教程: 使用跨平台布局引擎:https://archimboldi.me/posts/%E7%BF%BB%E8%AF%91-yoga-%E6%95%99%E7%A8%8B-%E4%BD%BF%E7%94%A8%E8%B7%A8%E5%B9%B3%E5%8F%B0%E5%B8%83%E5%B1%80%E5%BC%95%E6%93%8E.html
 
 #import "YogaKitViewController.h"
 #import <YogaKit/UIView+Yoga.h>
