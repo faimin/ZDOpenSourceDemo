2fcec9b6087d06d848b100828a5c4b98a6a604dd
faimin <fuxianchao@gmail.com>
2017-08-22 16:45:57 +0800

    RxSwift resource

--- ZDOpenSourceSwiftDemo/ZDOpenSourceSwiftDemo/Playgrounds/Playground_RxSwift.playground/Contents.swift
+++ ZDOpenSourceSwiftDemo/ZDOpenSourceSwiftDemo/Playgrounds/Playground_RxSwift.playground/Contents.swift
@@ -1,7 +1,17 @@
-//: Playground - noun: a place where people can play
+/*:
+ > RxSwift资料:
+ > - [RxSwift官方项目](https://github.com/ReactiveX/RxSwift)
+ > - [RxSwift入坑手册](https://blog.callmewhy.com/2015/09/21/rxswift-getting-started-0/)
+ > - [RxSwift入坑解读-你所需要知道的各种概念](http://www.codertian.com/2016/11/27/RxSwift-ru-keng-ji-read-document/)
+ > - [RxSwift入坑解读-那些难以理解的细节](http://www.codertian.com/2016/12/01/RxSwift-ru-keng-ji-learn-the-difficulty/)
+ > - [RxSwift实战教程-核心用法](http://www.codertian.com/2016/12/10/RxSwift-shi-zhan-jie-du-base-demo/)
+ > - [Getting Started With RxSwift and RxCocoa](http://southpeak.github.io/2017/01/16/Getting-Started-With-RxSwift-and-RxCocoa/)
+ */
 
 import UIKit
 import RxSwift
 import RxCocoa
 
 var str = "Hello, playground"
+
+let numberArr = [1, 2, 3, 4, 5]
--- ZDOpenSourceSwiftDemo/ZDOpenSourceSwiftDemo/Playgrounds/Playground_YogaKit.playground/Contents.swift
+++ ZDOpenSourceSwiftDemo/ZDOpenSourceSwiftDemo/Playgrounds/Playground_YogaKit.playground/Contents.swift
@@ -1,4 +1,4 @@
-//: Playground - noun: a place where people can play
+//: [Yoga 教程: 使用跨平台布局引擎](https://archimboldi.me/posts/%E7%BF%BB%E8%AF%91-yoga-%E6%95%99%E7%A8%8B-%E4%BD%BF%E7%94%A8%E8%B7%A8%E5%B9%B3%E5%8F%B0%E5%B8%83%E5%B1%80%E5%BC%95%E6%93%8E.html)
 
 import UIKit
 import LayoutKit
@@ -6,7 +6,7 @@ import YogaKit
 
 var str = "Hello, playground"
 
-//: YogaKit
+//: ### YogaKit
 let contentView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
 contentView.backgroundColor = .white
 contentView.configureLayout { (layout) in
