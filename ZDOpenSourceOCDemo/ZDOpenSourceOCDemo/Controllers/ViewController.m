e3324a3c4801e23c12f900ea7631724509e52e3d
faimin <fuxianchao@gmail.com>
2017-11-24 14:23:09 +0800

    peek&pop

--- ZDOpenSourceOCDemo/ZDOpenSourceOCDemo/Controllers/ViewController.m
+++ ZDOpenSourceOCDemo/ZDOpenSourceOCDemo/Controllers/ViewController.m
@@ -5,6 +5,9 @@
 //  Created by Zero.D.Saber on 2017/8/4.
 //  Copyright © 2017年 Zero.D.Saber. All rights reserved.
 //
+//  3D Touch实践讲解 --解决开发中的困惑点: https://www.tuicool.com/articles/VjEZniY
+//  3D Touch功能实现: https://www.tuicool.com/articles/qQZjuqF
+//  3Dtouch 的实际应用详解(tableView中的应用): https://www.tuicool.com/articles/baEnqef
 
 #import "ViewController.h"
 #import <objc/runtime.h>
@@ -12,7 +15,7 @@
 #import <ReactiveObjC/ReactiveObjC.h>
 #import <KZPlayground/KZPPlayground.h>
 
-@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate, UIViewControllerPreviewingDelegate>
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray<NSString *> *dataSource;
 @end
@@ -54,6 +57,9 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([UITableViewCell class]) forIndexPath:indexPath];
     cell.textLabel.text = self.dataSource[indexPath.row];
+    if (self.traitCollection.forceTouchCapability == UIForceTouchCapabilityAvailable) {
+        [self registerForPreviewingWithDelegate:self sourceView:cell];
+    }
     return cell;
 }
 
@@ -65,6 +71,42 @@
     [self.navigationController showViewController:[aClass new] sender:tableView];
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
+
+#pragma mark - UIViewControllerPreviewingDelegate
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPONE_9_0
+// peek
+- (UIViewController *)previewingContext:(id<UIViewControllerPreviewing>)previewingContext viewControllerForLocation:(CGPoint)location {
+    NSString *text = ((UITableViewCell *)previewingContext.sourceView).textLabel.text;
+    Class aClass = objc_getClass([text stringByAppendingString:@"ViewController"].UTF8String);
+    return [aClass new];
+}
+
+// pop
+- (void)previewingContext:(id<UIViewControllerPreviewing>)previewingContext commitViewController:(UIViewController *)viewControllerToCommit {
+    viewControllerToCommit.hidesBottomBarWhenPushed = YES;
+    [self showViewController:viewControllerToCommit sender:self];
+}
+
+// 预览时的底部菜单
+- (NSArray <id <UIPreviewActionItem>> *)previewActionItems {
+    UIPreviewAction *action1 = [UIPreviewAction actionWithTitle:@"标题1" style:UIPreviewActionStyleSelected handler:^(UIPreviewAction * _Nonnull action, UIViewController * _Nonnull previewViewController) {
+        NSLog(@"选中标题1");
+    }];
+    
+    UIPreviewAction *action2 = [UIPreviewAction actionWithTitle:@"标题2" style:UIPreviewActionStyleDefault handler:^(UIPreviewAction * _Nonnull action, UIViewController * _Nonnull previewViewController) {
+        NSLog(@"选中标题2");
+    }];
+    
+    UIPreviewAction *action3 = [UIPreviewAction actionWithTitle:@"标题3" style:UIPreviewActionStyleDestructive handler:^(UIPreviewAction * _Nonnull action, UIViewController * _Nonnull previewViewController) {
+        NSLog(@"选中标题3");
+    }];
+    
+    return @[action1, action2, action3];
+}
+#endif
+
+#pragma mark -
 
 - (IBAction)present:(UIBarButtonItem *)sender {
     [self.navigationController showViewController:[KZPPlaygroundViewController playgroundViewController] sender:sender];
