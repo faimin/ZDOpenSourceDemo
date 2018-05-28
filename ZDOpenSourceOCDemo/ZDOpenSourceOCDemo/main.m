af5d62dcc9086a53a07292e3e8321319f844ad45
faimin <fuxianchao@gmail.com>
2018-05-28 14:30:06 +0800

    hook msg_send

--- ZDOpenSourceOCDemo/ZDOpenSourceOCDemo/main.m
+++ ZDOpenSourceOCDemo/ZDOpenSourceOCDemo/main.m
@@ -9,8 +9,15 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+#if defined (__arm64__)
+void gohook();
+#endif
+
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+#if defined (__arm64__)
+        gohook();
+#endif
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
