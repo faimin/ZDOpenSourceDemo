e012484ea2622615231965ad95db5f9d8314164e
faimin <fuxianchao@gmail.com>
2019-02-22 12:10:35 +0800

    optional extension

---
+++ ZDOpenSourceSwiftDemo/ZDOpenSourceSwiftDemo/Category/ZDExtension.swift
@@ -0,0 +1,25 @@
+//
+//  ZDExtension.swift
+//  ZDOpenSourceSwiftDemo
+//
+//  Created by Zero.D.Saber on 2019/1/23.
+//  Copyright © 2019 Zero.D.Saber. All rights reserved.
+//
+
+import Foundation
+
+//MARK: - URL Extension
+extension URL: ExpressibleByStringLiteral {
+    public init(stringLiteral value: String) {
+        self = URL(string: value)!
+    }
+}
+//let url: URL = "www.google.com"
+
+//MARK: - Optional Extension
+//https://www.objc.io/blog/2019/01/22/non-empty-optionals/
+extension Optional where Wrapped: Collection {
+    var nonEmpty: Wrapped? {
+        return self?.isEmpty == true ? nil : self
+    }
+}
